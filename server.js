8334ac658c75d5e0eca4b28d537126a855eeebb0
fixgramwork <kimminje661@outlook.kr>
2024-07-16 08:23:54 +0900

    2st commit

--- server.js
+++ server.js
@@ -1,45 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const mysql = require('mysql');
-const authRoutes = require('./routes/authRoutes');
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'TP'
+});
 
-const mysql = require('mysql');
-const conn = {
-    host : 'localhost',
-    port : '3306',
-    user : 'root',
-    password : '',
-    database : 'TP'
-};
-
-var connection = mysql.createConnection(conn);
 connection.connect();
 
-var testQuery = "INSERT INTO `members` (`username`,`password`) VALUES ('test','test')'";
- 
-
-// 데이터베이스 연결
-db.connect((err) => {
-    if (err) {
-        console.error('데이터베이스 연결 오류:', err);
-        process.exit(1);
-    } else {
-        console.log('데이터베이스에 연결되었습니다.');
-    }
+connection.query('SELECT * from users', (error, rows, fields) => {
+  if (error) throw error;
+  console.log('User info is: ', rows);
 });
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// 미들웨어 설정
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 인증 라우트 사용
-app.use('/authRoutes', authRoutes(db));
-
-// 서버 시작
-app.listen(PORT, () => {
-    console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
-});
-
+connection.end();
